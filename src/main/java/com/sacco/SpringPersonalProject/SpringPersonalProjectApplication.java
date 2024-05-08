42905b8b076b283339a335ad446d9db3948a0304
SaccoUnibg <c.sacco@studenti.unibg.it>
2024-05-09 01:07:03 +0200

    first commit

---
+++ src/main/java/com/sacco/SpringPersonalProject/SpringPersonalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sacco.SpringPersonalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPersonalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sacco/SpringPersonalProject/SpringPersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sacco.SpringPersonalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
